6e726ff45bd8363f79df2513d266dd0b4c7e3650
Newton Ni <nwtnni@gmail.com>
2018-12-05 07:20:12 -0500

    Implement Vector3 operations with macros

--- src/math/macros.rs
+++ src/math/macros.rs
@@ -137,3 +137,143 @@ macro_rules! impl_vs2 {
         }
     };
 }
+
+macro_rules! impl_vv3 {
+    ($trait:ident, $trait_mut:ident, $method:ident, $method_mut:ident, $pat:pat => $fn:expr) => {
+        impl<N: Num> $trait for Vector3<N> {
+            type Output = Vector3<N>;
+            fn $method(self, rhs: Self) -> Self::Output {
+                let Vector3 { x: x1, y: y1, z: z1 } = self;
+                let Vector3 { x: x2, y: y2, z: z2 } = rhs;
+                let (x, y, z) = match ((x1, y1, z1), (x2, y2, z2)) {
+                    $pat => $fn,
+                };
+                Vector3 { x, y, z }
+            }
+        }
+
+        impl<N: Num> $trait<&Self> for Vector3<N> {
+            type Output = Vector3<N>;
+            fn $method(self, rhs: &Self) -> Self::Output {
+                let Vector3 { x: x1, y: y1, z: z1 } = self;
+                let Vector3 { x: x2, y: y2, z: z2 } = rhs;
+                let (x, y, z) = match ((x1, y1, z1), (*x2, *y2, *z2)) {
+                    $pat => $fn,
+                };
+                Vector3 { x, y, z }
+            }
+        }
+
+        impl<N: Num> $trait<&mut Self> for Vector3<N> {
+            type Output = Vector3<N>;
+            fn $method(self, rhs: &mut Self) -> Self::Output {
+                let Vector3 { x: x1, y: y1, z: z1 } = self;
+                let Vector3 { x: x2, y: y2, z: z2 } = rhs;
+                let (x, y, z) = match ((x1, y1, z1), (*x2, *y2, *z2)) {
+                    $pat => $fn,
+                };
+                Vector3 { x, y, z }
+            }
+        }
+
+        impl<N: Num> $trait_mut for Vector3<N> {
+            fn $method_mut(&mut self, rhs: Self) {
+                let Vector3 { x: x1, y: y1, z: z1 } = *self;
+                let Vector3 { x: x2, y: y2, z: z2 } = rhs;
+                let (x, y, z) = match ((x1, y1, z1), (x2, y2, z2)) {
+                    $pat => $fn,
+                };
+                *self = Vector3 { x, y, z }
+            }
+        }
+
+        impl<N: Num> $trait_mut<&Self> for Vector3<N> {
+            fn $method_mut(&mut self, rhs: &Self) {
+                let Vector3 { x: x1, y: y1, z: z1 } = *self;
+                let Vector3 { x: x2, y: y2, z: z2 } = *rhs;
+                let (x, y, z) = match ((x1, y1, z1), (x2, y2, z2)) {
+                    $pat => $fn,
+                };
+                *self = Vector3 { x, y, z }
+            }
+        }
+
+        impl<N: Num> $trait_mut<&mut Self> for Vector3<N> {
+            fn $method_mut(&mut self, rhs: &mut Self) {
+                let Vector3 { x: x1, y: y1, z: z1 } = *self;
+                let Vector3 { x: x2, y: y2, z: z2 } = *rhs;
+                let (x, y, z) = match ((x1, y1, z1), (x2, y2, z2)) {
+                    $pat => $fn,
+                };
+                *self = Vector3 { x, y, z}
+            }
+        }
+    };
+}
+
+macro_rules! impl_vs3 {
+    ($trait:ident, $trait_mut:ident, $method:ident, $method_mut:ident, $pat:pat => $fn:expr) => {
+        impl<N: Num> $trait<N> for Vector3<N> {
+            type Output = Vector3<N>;
+            fn $method(self, rhs: N) -> Self::Output {
+                let Vector3 { x, y, z } = self;
+                let (x, y, z) = match ((x, y, z), rhs) {
+                    $pat => $fn,
+                };
+                Vector3 { x, y, z }
+            }
+        }
+
+        impl<N: Num> $trait<&N> for Vector3<N> {
+            type Output = Vector3<N>;
+            fn $method(self, rhs: &N) -> Self::Output {
+                let Vector3 { x, y, z } = self;
+                let (x, y, z) = match ((x, y, z), *rhs) {
+                    $pat => $fn,
+                };
+                Vector3 { x, y, z }
+            }
+        }
+
+        impl<N: Num> $trait<&mut N> for Vector3<N> {
+            type Output = Vector3<N>;
+            fn $method(self, rhs: &mut N) -> Self::Output {
+                let Vector3 { x, y, z } = self;
+                let (x, y, z) = match ((x, y, z), *rhs) {
+                    $pat => $fn,
+                };
+                Vector3 { x, y, z }
+            }
+        }
+
+        impl<N: Num> $trait_mut<N> for Vector3<N> {
+            fn $method_mut(&mut self, rhs: N) {
+                let Vector3 { x, y, z } = *self;
+                let (x, y, z) = match ((x, y, z), rhs) {
+                    $pat => $fn,
+                };
+                *self = Vector3 { x, y, z }
+            }
+        }
+
+        impl<N: Num> $trait_mut<&N> for Vector3<N> {
+            fn $method_mut(&mut self, rhs: &N) {
+                let Vector3 { x, y, z } = *self;
+                let (x, y, z) = match ((x, y, z), *rhs) {
+                    $pat => $fn,
+                };
+                *self = Vector3 { x, y, z }
+            }
+        }
+
+        impl<N: Num> $trait_mut<&mut N> for Vector3<N> {
+            fn $method_mut(&mut self, rhs: &mut N) {
+                let Vector3 { x, y, z } = *self;
+                let (x, y, z) = match ((x, y, z), *rhs) {
+                    $pat => $fn,
+                };
+                *self = Vector3 { x, y, z }
+            }
+        }
+    };
+}
--- src/math/vector.rs
+++ src/math/vector.rs
@@ -1,13 +1,20 @@
 #![allow(dead_code)]
 
 use std::ops::{
-    Add, AddAssign, Deref, DerefMut, Div, DivAssign, Index, IndexMut, Mul, MulAssign, Sub,
-    SubAssign,
+    Add, AddAssign,
+    Sub, SubAssign,
+    Mul, MulAssign,
+    Div, DivAssign,
 };
 
 use noisy_float::prelude::*;
 
 use crate::math::Num;
+
+pub type Vector2i = Vector2<u32>;
+pub type Vector2f = Vector2<N32>;
+pub type Vector3i = Vector3<u32>;
+pub type Vector3f = Vector3<N32>;
 
 #[derive(Copy, Clone, Default, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Vector2<N: Num> {
@@ -32,7 +39,12 @@ pub struct Vector3<N: Num> {
     pub z: N,
 }
 
-pub type Vector2i = Vector2<u32>;
-pub type Vector2f = Vector2<N32>;
-pub type Vector3i = Vector3<u32>;
-pub type Vector3f = Vector3<N32>;
+impl_vv3!(Add, AddAssign, add, add_assign, ((x1, y1, z1), (x2, y2, z2)) => (x1 + x2, y1 + y2, z1 + z2));
+impl_vv3!(Sub, SubAssign, sub, sub_assign, ((x1, y1, z1), (x2, y2, z2)) => (x1 - x2, y1 - y2, z1 + z2));
+impl_vv3!(Mul, MulAssign, mul, mul_assign, ((x1, y1, z1), (x2, y2, z2)) => (x1 * x2, y1 * y2, z1 * z2));
+impl_vv3!(Div, DivAssign, div, div_assign, ((x1, y1, z1), (x2, y2, z2)) => (x1 / x2, y1 / y2, z1 / z2));
+
+impl_vs3!(Add, AddAssign, add, add_assign, ((x, y, z), s) => (x + s, y + s, z + s));
+impl_vs3!(Sub, SubAssign, sub, sub_assign, ((x, y, z), s) => (x - s, y - s, z - s));
+impl_vs3!(Mul, MulAssign, mul, mul_assign, ((x, y, z), s) => (x * s, y * s, z * s));
+impl_vs3!(Div, DivAssign, div, div_assign, ((x, y, z), s) => (x / s, y / s, z / s));
