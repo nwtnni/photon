c0f8dcbc1c401eae97a20436a2408fb1e688dfa6
Newton Ni <nwtnni@gmail.com>
2019-02-26 16:09:44 -0500

    Update camera to take in FOV and aspect ratio

--- src/camera.rs
+++ src/camera.rs
@@ -8,7 +8,14 @@ pub struct Camera {
 }
 
 impl Camera {
-    pub fn new(corner: Vec3, horizontal: Vec3, vertical: Vec3, origin: Vec3) -> Self {
+    pub fn new(fov: f32, aspect: f32) -> Self {
+        let theta = fov * std::f32::consts::PI / 180.0;
+        let height = (theta / 2.0).tan(); 
+        let width = aspect * height;
+        let corner = Vec3::new(-width, -height, -1.0);
+        let horizontal = Vec3::new(2.0 * width, 0.0, 0.0);
+        let vertical = Vec3::new(0.0, 2.0 * height, 0.0);
+        let origin = Vec3::default();
         Camera { corner, horizontal, vertical, origin }
     }
 
