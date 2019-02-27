bef079aaf1c2448992739e58e6797d522e1c62bc
Newton Ni <nwtnni@gmail.com>
2019-02-26 22:22:27 -0500

    Implement random scene generation

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::collections::HashMap;
 use rayon::prelude::*;
 
 use photon::geometry::{Ray, Vec3};
-use photon::material::{Diffuse, Metal, Dielectric};
+use photon::material::{Material, Diffuse, Metal, Dielectric};
 use photon::surface::{Surface, Sphere, List, Hit};
 use photon::camera::Camera;
 use photon::preview::Preview;
@@ -29,8 +29,8 @@ fn color(ray: &Ray, scene: &Surface, depth: i32) -> Vec3 {
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let nx = 1600;
-    let ny = 800;
+    let nx = 1920;
+    let ny = 1080;
     let ns = 100;
 
     let mut ppm = PPM::new(nx, ny, "test.ppm")?;
@@ -38,37 +38,83 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let preview = Preview::new(nx, ny, rx);
     let handle = std::thread::spawn(|| preview.run());
 
-    let origin = Vec3::new(3.0, 3.0, 2.0);
-    let toward = Vec3::new(0.0, 0.0, -1.0);
+    let origin = Vec3::new(10.0, 3.0, 10.0);
+    let toward = Vec3::new(0.0, 0.0, 0.0);
     let up = Vec3::new(0.0, 1.0, 0.0);
-    let focus = (origin - toward).len();
-    let aperture = 1.0;
-
-    let camera = Camera::new(
-        origin,
-        toward,
-        up,
-        20.0,
-        nx as f32 / ny as f32,
-        aperture,
-        focus,
-    );
-
-    let da = Diffuse::new(Vec3::new(0.8, 0.3, 0.3));
-    let db = Diffuse::new(Vec3::new(0.8, 0.8, 0.0));
-    let ga = Dielectric::new(1.5);
-    let mb = Metal::new(Vec3::new(0.8, 0.6, 0.2), 1.0);
-
-    let small = Sphere::new(Vec3::new(0.0, 0.0, -1.0), 0.5, &da);
-    let large = Sphere::new(Vec3::new(0.0, -100.5, -1.0), 100.0, &db);
-    let left = Sphere::new(Vec3::new(-1.0, 0.0, -1.0), 0.5, &ga);
-    let right = Sphere::new(Vec3::new(1.0, 0.0, -1.0), 0.5, &mb);
+    let focus = 12.0;
+    let aperture = 0.5;
+    let camera = Camera::new(origin, toward, up, 20.0, nx as f32 / ny as f32, aperture, focus);
 
     let mut scene = List::default();
-    scene.push(&small);
-    scene.push(&large);
-    scene.push(&left);
-    scene.push(&right);
+    let mut diffuse = Vec::new();
+    let mut dielectric = Vec::new();
+    let mut metallic = Vec::new();
+    let mut spheres = Vec::new();
+    let mut materials = Vec::new();
+    let mut centers = Vec::new();
+
+    macro_rules! rand { () => { rand::random::<f32>() } };
+
+    for x in (-10..=10).map(|x| x as f32) {
+        for y in (-10..=10).map(|y| y as f32) {
+            let material_chance = rand::random::<f32>();
+            let center = Vec3::new(x + 0.9 * rand!(), 0.2, y + 0.9 * rand!());
+            if (center - Vec3::new(4.0, 0.2, 0.0)).len() > 0.9 {
+                centers.push(center);
+                if material_chance < 0.8 {
+                    diffuse.push(Diffuse::new(Vec3::new(
+                        rand!() * rand!(),
+                        rand!() * rand!(),
+                        rand!() * rand!(),
+                    )));
+                    materials.push(0u8);
+                } else if material_chance < 0.95 {
+                    metallic.push(Metal::new(
+                        Vec3::new(
+                            (rand!() + 1.0) * 0.5,
+                            (rand!() + 1.0) * 0.5,
+                            (rand!() + 1.0) * 0.5,
+                        ),
+                        rand!() * 0.5,
+                    ));
+                    materials.push(1u8);
+                } else {
+                    dielectric.push(Dielectric::new(rand!() * 2.0));
+                    materials.push(2u8);
+                }
+            }
+        }
+    }
+
+    let mut diffuse_iter = diffuse.iter();
+    let mut dielectric_iter = dielectric.iter();
+    let mut metallic_iter = metallic.iter();
+    for (m, c) in materials.into_iter().zip(centers.into_iter()) {
+        let material = match m {
+        | 0 => diffuse_iter.next().unwrap() as &dyn Material,
+        | 1 => metallic_iter.next().unwrap() as &dyn Material,
+        | 2 => dielectric_iter.next().unwrap() as &dyn Material,
+        | _ => unreachable!(),
+        };
+        spheres.push(Sphere::new(c, 0.2, material));
+    }
+
+    for sphere in &spheres { scene.push(sphere); }
+
+    let gray = Diffuse::new(Vec3::new(0.5, 0.5, 0.5));
+    let diffuse = Diffuse::new(Vec3::new(0.2, 0.5, 0.6));
+    let glass = Dielectric::new(1.5);
+    let metal = Metal::new(Vec3::new(0.7, 0.6, 0.5), 0.0);
+
+    let floor = Sphere::new(Vec3::new(0.0, -1000.0, 0.0), 1000.0, &gray);
+    let far = Sphere::new(Vec3::new(-4.0, 1.0, 0.0), 1.0, &diffuse);
+    let mid = Sphere::new(Vec3::new(0.0, 1.0, 0.0), 1.0, &glass);
+    let close = Sphere::new(Vec3::new(4.0, 1.0, 0.0), 1.0, &metal);
+
+    scene.push(&floor);
+    scene.push(&far);
+    scene.push(&mid);
+    scene.push(&close);
 
     let mut buffers: HashMap<usize, Vec<(u8, u8, u8)>> = HashMap::with_capacity(ny);
     buffers.par_extend((0..ny).into_par_iter().map(move |y| {
