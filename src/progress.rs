76b3b38fbac89ebca66cb44295b0a323fa9c85f1
Newton Ni <nwtnni@gmail.com>
2019-03-03 19:20:12 -0500

    Add elapsed to progress bar

--- src/progress.rs
+++ src/progress.rs
@@ -25,29 +25,36 @@ impl Spinner {
 impl std::fmt::Display for Spinner {
     fn fmt(&self, fmt: &mut std::fmt::Formatter) -> std::fmt::Result {
         let spinner = match self {
-        | Spinner::A => '◷',
-        | Spinner::B => '◶',
-        | Spinner::C => '◵',
-        | Spinner::D => '◴', 
+        | Spinner::A => '-',
+        | Spinner::B => '\\',
+        | Spinner::C => '|',
+        | Spinner::D => '/', 
         };
         write!(fmt, "{}", spinner)
     }
 }
 
 pub fn run(total: usize) -> Result<(), std::io::Error> {
+    let start = std::time::Instant::now();
     let stdout = std::io::stdout();
     let mut out = stdout.lock();
     let mut spinner = Spinner::A;
     write!(out, "{}", termion::cursor::Save)?;
     loop {
-        write!(out, "{}", termion::cursor::Restore)?; 
+        let now = std::time::Instant::now();
+        let mut span = now.duration_since(start).as_secs();
+        let h = span / 3600; span %= 3600;
+        let m = span / 60;   span %= 60;
+        let s = span;
         let rendered = PIXELS_RENDERED.read();
         let done = rendered * BARS / total;
         let rest = BARS - done;
         write!(
             out,
-            "[{}] Rendering... | [{}{}] | {:.2}% | {} out of {} pixels",
+            "{}[{}] | Elapsed: {:0>2}:{:0>2}:{:0>2} | [{}{}] | {:.2}% | {} out of {} pixels",
+            termion::cursor::Restore,
             spinner,
+            h, m, s,
             DONE.repeat(done),
             REST.repeat(rest),
             100.0 * rendered as f32 / total as f32,
@@ -55,8 +62,6 @@ pub fn run(total: usize) -> Result<(), std::io::Error> {
             total,
         )?;
         spinner.rotate();
-        std::thread::sleep(
-            std::time::Duration::from_millis(100)
-        );
+        std::thread::sleep(std::time::Duration::from_millis(100));
     }
 }
