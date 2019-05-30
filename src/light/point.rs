6968a850763ec40485a6b4dea62c1395a58826ee
Newton Ni <nwtnni@gmail.com>
2019-05-29 20:06:19 -0400

    Rename point fields

--- src/light/point.rs
+++ src/light/point.rs
@@ -3,19 +3,25 @@ use crate::light::Light;
 
 #[derive(Copy, Clone, Debug)]
 pub struct Point {
-    position: Vec3,
-    intensity: Vec3,
+    /// Position
+    p: Vec3,
+
+    /// Intensity
+    i: Vec3,
 }
 
 impl Point {
     pub fn new(position: Vec3, intensity: Vec3) -> Self {
-        Point { position, intensity }
+        Point {
+            p: position,
+            i: intensity,
+        }
     }
 }
 
 impl Light for Point {
-    fn sample(&self, point: &Vec3, wi: &mut Vec3) -> f32 {
-        *wi = (self.position - point).normalize();
+    fn sample(&self, p: &Vec3, wi: &mut Vec3) -> f32 {
+        *wi = (self.p - p).normalize();
         1.0
     }
 }
