150687342a34fbae7bb599cb8b8c7fbf502629cf
Newton Ni <nwtnni@gmail.com>
2019-06-05 19:55:20 -0400

    Implement normal integrator for debugging

--- src/integrator.rs
+++ src/integrator.rs
@@ -2,6 +2,10 @@ use crate::geometry::{Ray, Vec3};
 use crate::surface::Hit;
 use crate::scene;
 
+mod normal;
+
+pub use normal::Normal;
+
 pub trait Integrator<'scene> {
-    fn shade(scene: &scene::Scene<'scene>, ray: &Ray, hit: &Hit<'scene>, depth: usize) -> Vec3;
+    fn shade(scene: &scene::Scene<'scene>, ray: &Ray, hit: &Hit<'scene>) -> Vec3;
 }
---
+++ src/integrator/normal.rs
@@ -0,0 +1,17 @@
+use crate::geometry::{Ray, Vec3};
+use crate::integrator;
+use crate::scene;
+use crate::surface::Hit;
+
+#[derive(Copy, Clone, Debug)]
+pub struct Normal;
+
+impl<'scene> integrator::Integrator<'scene> for Normal {
+    fn shade(_: &scene::Scene<'scene>, _: &Ray, hit: &Hit<'scene>) -> Vec3 {
+        Vec3::new(
+            (hit.n.x() + 1.0) / 2.0,
+            (hit.n.y() + 1.0) / 2.0,
+            (hit.n.z() + 1.0) / 2.0,
+        )
+    }
+}
