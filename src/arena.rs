4e5f946892673cc460fdcfdf9126d95998700a02
Newton Ni <nwtnni@gmail.com>
2019-06-10 13:23:50 -0400

    Align item before allocating

--- src/arena.rs
+++ src/arena.rs
@@ -3,6 +3,8 @@ use std::cell;
 
 use crate::stats;
 
+const ALIGN: usize = 8;
+
 #[derive(Debug)]
 pub struct Arena {
     buf: *mut u8,
@@ -12,16 +14,18 @@ pub struct Arena {
 
 impl Arena {
     pub fn new(capacity: usize) -> Self {
+        let cap = capacity;
+        let len = cell::Cell::new(0);
+        let lay = alloc::Layout::from_size_align(cap, ALIGN).unwrap();
         unsafe {
-            let cap = capacity;
-            let len = cell::Cell::new(0);
-            let buf = alloc::alloc(alloc::Layout::from_size_align_unchecked(cap, 8));
+            let buf = alloc::alloc(lay);
             Arena { cap, len, buf }
         }
     }
 
     pub fn alloc<T: Copy>(&self, item: T) -> &T {
         let size = std::mem::size_of::<T>();
+        let size = (size + ALIGN - 1) & !(ALIGN - 1);
         let len = self.len.get();
         if len + size >= self.cap { panic!("[INTERNAL ERROR]: Arena ran out of memory"); }
         self.len.set(len + size);
