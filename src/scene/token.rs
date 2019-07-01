6516d36cf20d8ee2cbb9a7c97c2897da1e3c9568
Newton Ni <nwtnni@gmail.com>
2019-07-01 14:03:43 -0400

    Implement basic tokens

--- src/scene/token.rs
+++ src/scene/token.rs
@@ -1,3 +1,22 @@
+#[derive(Clone, Debug)]
 pub enum Token {
+    String(String),
     Float(f32),
+
+    Scene,
+    Camera,
+    Integrator,
+
+    Surface,
+    Light,
+ 
+    Sphere,
+    Quad,
+    Point,
+    Mesh,
+
+    Glazed,
+    Mirror,
+    Lambertian,
+    Specular,
 }
