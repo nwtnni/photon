ae32c8ae74bd8c0203c65096536729dc10af15f6
Newton Ni <nwtnni@gmail.com>
2019-07-22 23:32:14 -0400

    Remove texture module for now

--- src/lib.rs
+++ src/lib.rs
@@ -37,13 +37,9 @@ pub mod geom;
 /// Statistic tracking
 pub mod stats;
 
-/// Coloring textures
-pub mod texture;
-
 pub mod prelude {
     pub use crate::bxdf::BxDF;
     pub use crate::integrator::Integrator;
     pub use crate::light::Light;
     pub use crate::geom::Surface;
-    pub use crate::texture::Texture;
 }
