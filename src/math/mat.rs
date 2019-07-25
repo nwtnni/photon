fd66ef836b1de281b0fb596ed211f458442e349c
Newton Ni <nwtnni@gmail.com>
2019-07-25 14:51:34 -0400

    Implement scale constructors

--- src/math/mat.rs
+++ src/math/mat.rs
@@ -14,29 +14,35 @@ impl Mat4 {
     }
 
     pub fn translate_x(x: f32) -> Self {
-        Mat4([
-            1.0, 0.0, 0.0, x,
-            0.0, 1.0, 0.0, 0.0,
-            0.0, 0.0, 1.0, 0.0,
-            0.0, 0.0, 0.0, 1.0,
-        ])
+        Self::translate(math::Vec3::new(x, 0.0, 0.0))
     }
 
     pub fn translate_y(y: f32) -> Self {
+        Self::translate(math::Vec3::new(0.0, y, 0.0))
+    }
+
+    pub fn translate_z(z: f32) -> Self {
+        Self::translate(math::Vec3::new(0.0, 0.0, z))
+    }
+
+    pub fn scale(s: math::Vec3) -> Self {
         Mat4([
-            1.0, 0.0, 0.0, 0.0,
-            0.0, 1.0, 0.0, y,
-            0.0, 0.0, 1.0, 0.0,
+            s.x(), 0.0, 0.0, 0.0,
+            0.0, s.y(), 0.0, 0.0,
+            0.0, 0.0, s.z(), 0.0,
             0.0, 0.0, 0.0, 1.0,
         ])
     }
 
-    pub fn translate_z(z: f32) -> Self {
-        Mat4([
-            1.0, 0.0, 0.0, 0.0,
-            0.0, 1.0, 0.0, 0.0,
-            0.0, 0.0, 1.0, z,
-            0.0, 0.0, 0.0, 1.0,
-        ])
+    pub fn scale_x(s: f32) -> Self {
+        Self::scale(math::Vec3::new(s, 0.0, 0.0))
+    }
+
+    pub fn scale_y(s: f32) -> Self {
+        Self::scale(math::Vec3::new(0.0, s, 0.0))
+    }
+
+    pub fn scale_z(s: f32) -> Self {
+        Self::scale(math::Vec3::new(0.0, 0.0, s))
     }
 }
